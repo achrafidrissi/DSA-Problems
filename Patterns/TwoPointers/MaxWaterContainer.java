3dd10d1a05292a1578ab3c9aee64a55d0cffa011
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-04-15 16:17:01 +0100

    Container With Most Water

---
+++ Patterns/TwoPointers/MaxWaterContainer.java
@@ -0,0 +1,23 @@
+package Patterns.TwoPointers;
+
+// =====================================================
+// 11. Container With Most Water
+// =====================================================
+
+public class MaxWaterContainer {
+    public int maxArea(int[] height){
+        int left = 0;
+        int right = height.length-1;
+        int maxArea = 0;
+        while (left<right){
+            if(height[left] < height[right]){
+                maxArea = Math.max(height[left]  * (right-left), maxArea);
+                left++;
+            }else {
+                maxArea = Math.max(height[right]  * (right-left), maxArea);
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
