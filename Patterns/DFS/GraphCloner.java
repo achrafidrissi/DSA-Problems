03c846ddf74e1167d4c9aa0f144a130c0b7611ba
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-04-19 09:47:08 +0100

    Clone Graph

---
+++ Patterns/DFS/GraphCloner.java
@@ -0,0 +1,30 @@
+package Patterns.DFS;
+
+// =====================================================
+// 133. Clone Graph
+// =====================================================
+
+
+import java.util.*;
+
+public class GraphCloner {
+    private Map<Node, Node> visited = new HashMap<>();
+    public Node cloneGraph(Node node){
+        if(node==null){
+            return null;
+        }
+
+        if(visited.containsKey(node)){
+            return visited.get(node);
+        }
+
+        Node cloneNode = new Node(node.val);
+        visited.put(node, cloneNode);
+
+        for (Node neighbor : node.neighbors ){
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return cloneNode;
+    }
+}
