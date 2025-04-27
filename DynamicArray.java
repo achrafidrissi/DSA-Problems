844517d033aabed65ef4516110a76496cf144b56
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-04-27 11:21:14 +0100

    DynamicArray

---
+++ DynamicArray.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DynamicArray {
+    public static List<Integer> dynamicArray(int n, List<List<Integer>> queries){
+        List<List<Integer>> list  = new ArrayList<>();
+        List<Integer> answers = new ArrayList<>();
+        int lastAnswer = 0;
+
+        for (int i = 0; i<n ; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i<queries.size(); i++){
+            int index = (queries.get(i).get(1) ^ lastAnswer) % n;
+            if(queries.get(i).get(0) == 1){
+                list.get(index).add(queries.get(i).get(2));
+            }else if (queries.get(i).get(0) == 2){
+                int temp = queries.get(i).get(2) % list.get(index).size();
+                lastAnswer = list.get(index).get(temp);
+                answers.add(lastAnswer);
+            }
+        }
+    return answers;
+    }
+}
