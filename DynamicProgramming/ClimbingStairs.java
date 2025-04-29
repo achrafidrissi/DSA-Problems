715f06ac06b11dd1ab4e9239acc24cc1982fd17b
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-04-29 12:32:56 +0100

    Climbing Stairs

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -1,2 +1,17 @@
-package DynamicProgramming;public class ClimbingStairs {
+package DynamicProgramming;
+
+// =====================================================
+// 70. Climbing Stairs
+// =====================================================
+
+public class ClimbingStairs {
+    public int climbStairs(int n){
+        int first = 1, second = 1;
+        for(int i = 2; i<=n; i++){
+            int temp = second;
+            second = first+second;
+            first = temp;
+        }
+        return second;
+    }
 }
