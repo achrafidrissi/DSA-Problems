3a10a7bdf52cf69faebaadcffdacb202dcff55e1
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-05-18 20:43:01 +0100

    Single Number

---
+++ Bitset/SingleNumber.java
@@ -0,0 +1,16 @@
+package Bitset;
+
+// =====================================================
+// 136. Single Number
+// =====================================================
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums){
+        int result = 0;
+        for (int num : nums){
+            result ^= num;
+        }
+        return  result;
+    }
+}
