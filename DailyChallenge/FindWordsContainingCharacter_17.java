3d5a4e9e42f75ecbd33fdcf67c2490a86d40469b
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-05-24 21:16:03 +0100

    Find Words Containing Character

---
+++ DailyChallenge/FindWordsContainingCharacter_17.java
@@ -0,0 +1,22 @@
+package DailyChallenge;
+
+// =====================================================
+// 2942. Find Words Containing Character
+// =====================================================
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindWordsContainingCharacter_17 {
+
+    List<Integer> findWordsContaining(String[] words, char x){
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0 ; i < words.length; i++){
+            if(words[i].indexOf(x) != -1){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
